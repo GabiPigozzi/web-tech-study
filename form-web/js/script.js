b3abcf0e80b15a29e87a49a96b1582b4df14e776
Gabriel Pigozzi <164095675+GabiPigozzi@users.noreply.github.com>
2026-06-03 11:48:21 -0300

    fiz sozinho

--- form-web/js/script.js
+++ form-web/js/script.js
@@ -1,6 +1,8 @@
 //criando váriaveis
 
 var nome = document.querySelector("#nome");
+var cpf = document.querySelector("#cpf");
+var telefone = document.querySelector("#telefone")
 var button = document.querySelector("button");
 var lista = document.querySelector(".lista");
 
@@ -8,9 +10,20 @@ var lista = document.querySelector(".lista");
 
 button.addEventListener("click", (event) => {
     event.preventDefault();
-    
-    console.log(nome.value)
-
+    const inputValeuNome = nome.value;
+    const inputValeucpf = cpf.value;
+    const inputValeutelefone = telefone.value;
+    const templateHTML = `<li><p>${inputValeuNome}</p>
+                              <p>${inputValeucpf}</p>
+                             <p> ${inputValeutelefone}</p></li>`;
   //incluir itens no html
+
+  lista.innerHTML += templateHTML;
+
+  //limpando campo
+
+  nome.value ="";
+  cpf.value="";
+  telefone.value="";
 
 })
