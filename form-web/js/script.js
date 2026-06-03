efd955d21876a8ffb37f7caeb9f4ac7857289d51
Gabriel Pigozzi <164095675+GabiPigozzi@users.noreply.github.com>
2026-06-03 11:16:16 -0300

    feat: adicionando javascript

---
+++ form-web/js/script.js
@@ -0,0 +1,16 @@
+//criando váriaveis
+
+var nome = document.querySelector("#nome");
+var button = document.querySelector("button");
+var lista = document.querySelector(".lista");
+
+//criando função
+
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+    
+    console.log(nome.value)
+
+  //incluir itens no html
+
+})
