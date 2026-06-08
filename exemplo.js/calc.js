1d82a19a6fe19e2aa21c06bee46dfdfb346083f4
Gabriel Pigozzi <164095675+GabiPigozzi@users.noreply.github.com>
2026-06-08 11:20:47 -0300

    feat: Apredendo a usar Let no JS

---
+++ exemplo.js/calc.js
@@ -0,0 +1,19 @@
+//variaveis
+let num1 = 10;
+let num2 = 40;
+
+//operações
+
+let soma = num1 + num2;
+let subtração = num1-num2;
+let multiplicacao = num1*num2;
+let divisao = num1/num2;
+
+//resultado
+
+console.log("soma ", soma);
+console.log("subtração ", subtração);
+console.log("multiplicação ", multiplicacao);
+console.log("divisão", divisao);
+
+
