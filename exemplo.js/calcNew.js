0b7b6337e8c113cd8bd23ba813177f62b26ff674
Gabriel Pigozzi <164095675+GabiPigozzi@users.noreply.github.com>
2026-06-08 11:49:57 -0300

    feat: criando mais funções

--- exemplo.js/calcNew.js
+++ exemplo.js/calcNew.js
@@ -1,6 +1,34 @@
-function meuNome(nome){
+/*function meuNome(nome){
     console.log(nome)
 }
 
 meuNome("Gabriel");
-meuNome("Giuliana");
+meuNome("Giuliana");*/
+
+function soma(num1, num2){
+    console.log(num1 + num2);
+}
+
+soma(10,10);
+soma(5,5);
+
+function subtração(num1, num2){
+    console.log(num1 - num2);
+
+}
+
+subtração(15,5);
+
+function multiplicação(num1, num2){
+    console.log(num1 * num2);
+
+}
+
+multiplicação(10,10)
+
+function divisão(num1, num2){
+    console.log(num1 / num2);
+
+}
+
+divisão(10,5)
