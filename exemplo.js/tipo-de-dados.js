a73a65b833ad7d2ec3ce6d93da1127baa1cf4e68
Gabriel Pigozzi <164095675+GabiPigozzi@users.noreply.github.com>
2026-06-08 10:45:40 -0300

    feat: Aprendendo sobre os tipods de dados

---
+++ exemplo.js/tipo-de-dados.js
@@ -0,0 +1,22 @@
+//string
+
+let nome = "Gabriel Pigozzi";
+
+//inteiro
+
+let idade = 21;
+
+//boolean
+
+let casado = true;
+
+//números flutuantes com casas decimais
+
+let valorProduto = 5.35;
+
+//Resultado
+console.log("Meu nome é " + nome);
+console.log("Minha idade é: " + idade);
+console.log("Casado? " + casado);
+console.log("R$", valorProduto);
+console.log(typeof nome);
